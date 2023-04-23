420b61e4e36690734ff861a9e410a4a1f15bee99
Ramon1108 <122237742+Ramon1108@users.noreply.github.com>
2023-04-24 00:05:13 +0300

    problem #1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
+// import * as basicLightbox from "basiclightbox";
 
-console.log(galleryItems);
+// const instance = basicLightbox.create(`
+//     <img src="assets/images/image.png" width="800" height="600">
+// `);
+
+// instance.show();
+
+const imgList = document.querySelector(".gallery");
+const imgMarkUp = createImg(galleryItems);
+
+imgList.insertAdjacentHTML("beforeend", imgMarkUp);
+
+function createImg(imgItems) {
+  return imgItems
+    .map((img) => {
+      return `
+    <li>
+     <a class = "gallery__link">
+      <img class = "gallery__image" src = ${img.preview} alt = ${img.description}/>
+     </a>
+    </li>`;
+    })
+    .join("");
+}
